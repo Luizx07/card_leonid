d3641ab5d6e467062785cb1b68fa57f733e423d0
LUIZ HENRIQUE DOS SANTOS <24122470@CFP127ALUNO.LOCAL>
2025-02-04 11:27:34 -0300

    aula

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict'
+
+/*const botaoTeste = document.getElementById('teste')
+
+function criarElementos (){
+    const body = document.querySelector('body')
+    const novoBotao = document.createElement('button')
+    novoBotao.textContent = 'botao criado pelo js'
+
+    body.appendChild(novoBotao)
+}
+//criarElementos()
+
+botaoTeste.addEventListener('click', criarElementos)*/
+
+const disciplinas = [
+    {nome: 'PWBE', cor: 'red', icon: 'pwbe.png'},
+    {nome: 'PWFW', cor: 'blue', icon: 'pwfe.png'},
+    {nome: 'PPDM', cor: 'yellow', icon: 'ppdm.png'},
+    {nome: 'PRO',  cor : 'pink', icon: 'pro.png'}
+]
+
+function criarItemMenu (disciplina) {
+    const listaMenu = document.getElementById('menu')
+    const novoItem = document.createElement('li')
+    const novoLink = document.createElement('a')
+    const novoImg = document.createElement('img')
+
+    novoLink.href = '#'
+    novoLink.textContent = disciplina.nome
+
+    novoLink.style = `--cor-hover: ${disciplina.cor}`
+
+    novoImg.src = `./icon/${disciplina.icon}`
+
+    novoItem.appendChild(novoImg)
+
+    novoItem.appendChild(novoLink)
+
+    listaMenu.appendChild(novoItem)
+}
+
+disciplinas.forEach(criarItemMenu)
