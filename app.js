09059157f448d0def79904d659e087db68a082e4
LUIZ HENRIQUE DOS SANTOS <24122470@CFP127ALUNO.LOCAL>
2025-02-11 11:52:30 -0300

    card html e css feito

--- app.js
+++ app.js
@@ -42,3 +42,35 @@ function criarItemMenu (disciplina) {
 
 disciplinas.forEach(criarItemMenu)
 
+const carros = [
+    {nome: 'Skyline R34', icon: 'sky.webp',descricao: 'O R34 foi o último dos Skyline GT-R, sucedendo o R32 e o R33 e formando a Santíssima Trindade da Nissan. Como os outros dois, ele tinha um seis-em-linha biturbo de 2,6 litros, cerca de 300 cv, tração integral e câmbio manual de seis marchas.'},
+    {nome: 'Supra', icon: 'supra2.jpg', descricao:'O Toyota Supra MK4 impressiona pela sua potência e desempenho. Seu motor é um 3.0 litros de seis cilindros em linha, com turbocompressor e injeção direta de combustível, proporcionando 326 cavalos de potência e 44 kgfm de torque.'},
+    {nome: 'Mazda Rx7', icon: 'mazda.webp', descricao: "Possui tração traseira e motor de até 280 cavalos de potência. Apesar de a Mazda ter parado de fabricá-lo, ainda é possível vê-lo em alguns salões de automóveis, jogos eletrônicos (como Need for Speed) e é frequentemente visto nas ruas do Japão."}
+]
+
+function criarCards(carros) {
+    carros.forEach(cars => {
+        const container = document.getElementById('container')
+
+        const nvcard = document.createElement('div')
+        nvcard.classList.add('card1')
+
+        const Titulo = document.createElement('h2')
+        Titulo.textContent = cars.nome
+
+        const Paragrafo = document.createElement('p')
+        Paragrafo.textContent = cars.descricao
+
+        const Img = document.createElement('img')
+        Img.src = `./icon/${cars.icon}`
+        Img.classList.add('card-img')
+
+        nvcard.appendChild(Img)
+        nvcard.appendChild(Titulo)
+        nvcard.appendChild(Paragrafo)
+
+        container.appendChild(nvcard)
+    })
+}
+
+criarCards(carros)
